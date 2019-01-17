d5ec11f0516f803434e444a295c37a8bd20cbf4a
Brad Gibson <b2b@humanenginuity.com>
2019-01-17 06:17:52 +0000

    Added OsString->String conversion error, display & conversion logic

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,30 @@
-use std::fmt::{
-    Display,
-    Formatter,
-    Result as FmtResult,
+use crate::consts::*;
+use std::{
+    ffi::OsString,
+    fmt::{
+        Display,
+        Formatter,
+        Result as FmtResult,
+    },
 };
 
 #[derive(Debug)]
-pub enum Error {}
+pub enum Error {
+    ArgNotConvertibleToUtf8(OsString),
+}
 
 impl Display for Error {
-    fn fmt(&self, _f: &mut Formatter) -> FmtResult {
-        unimplemented!();
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", match self {
+            Error::ArgNotConvertibleToUtf8(os_string) => format!("{}: {:?}",
+                                                                 msg::ERR_ARG_NOT_CONVERTIBLE_TO_UTF_8,
+                                                                 os_string),
+        })
+    }
+}
+
+impl From<OsString> for Error {
+    fn from(err: OsString) -> Self {
+        Error::ArgNotConvertibleToUtf8(err)
     }
 }
