e51603911db0da48511fec215c2d5094c62d3955
Brad Gibson <b2b@humanenginuity.com>
2022-11-11 06:19:24 -0800

    Add link to reference `no_std` `format` impl

--- src/error.rs
+++ src/error.rs
@@ -12,6 +12,7 @@ use crate::consts::*;
 pub enum Error {}
 
 impl Display for Error {
+    // TODO: Reimplement https://github.com/zartarn15/format_no_std/blob/master/src/lib.rs as `he_std` for `no_std` `format` capability
     fn fmt(&self, _f: &mut Formatter) -> FmtResult {
         unimplemented!()
     }
